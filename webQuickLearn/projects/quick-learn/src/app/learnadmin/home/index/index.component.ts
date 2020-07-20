6e36cb208dee885ea5547a1b223afe9222f41a03
Ale <az_ale@163.com>
2020-07-20 14:15:06 +0800

    响应式布局调整

--- webQuickLearn/projects/quick-learn/src/app/learnadmin/home/index/index.component.ts
+++ webQuickLearn/projects/quick-learn/src/app/learnadmin/home/index/index.component.ts
@@ -12,14 +12,14 @@ export class IndexComponent implements OnInit {
 
   public code?: string;
   isCollapsed: boolean = false;
-  style: string = '0 0 237px';
+  style: string = '0 0 200px';
   userImg: string = '../../../assets/imgs/users/user-h-img.jpg';
   visible = false;
-  openMap: { [name: string]: boolean } = {
-    sub1: true,
-    sub2: false,
-    sub3: false
-  };
+  // openMap: { [name: string]: boolean } = {
+  //   sub1: true,
+  //   sub2: false,
+  //   sub3: false
+  // };
 
   constructor(private drawerService: NzDrawerService,private modalService: NzModalService,private router: Router) {
     
@@ -49,22 +49,29 @@ export class IndexComponent implements OnInit {
 
 
 
-
+  /**
+   * 左侧比例设置
+   */
   doTypesetting() {
     this.isCollapsed = this.isCollapsed ? false : true;
-    this.style = this.isCollapsed ? '0 0 150px' : '0 0 300px'
+    this.style = this.isCollapsed ? '0 0 90px' : '0 0 200px'
   }
 
+  // /**
+  //  * 菜单的开启与关闭
+  //  * @param value 
+  //  */
+  // openHandler(value: string): void {
+  //   for (const key in this.openMap) {
+  //     if (key !== value) {
+  //       this.openMap[key] = false;
+  //     }
+  //   }
+  // }
 
-  openHandler(value: string): void {
-    for (const key in this.openMap) {
-      if (key !== value) {
-        this.openMap[key] = false;
-      }
-    }
-  }
-
-
+  /**
+   * 弹出个人信息框
+   */
   openUserInfo() {
     
     // console.log('headClick()');
