11d9beeb4527039bb7045e65b07c5b74bf1143fc
Ale <az_ale@163.com>
2020-07-20 16:55:59 +0800

    引入LocationStrategy策略

--- webQuickLearn/projects/quick-learn/src/app/app.component.ts
+++ webQuickLearn/projects/quick-learn/src/app/app.component.ts
@@ -10,9 +10,32 @@ export class AppComponent {
   title = 'quick-learn';
   isLogin: boolean = true;
 
+  showLoading: boolean = true;
+
+  connectModel: string = 'http';
+
   constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    this.router.navigateByUrl(this.isLogin ? '/learnadmin' : '/learnlogin');
+    /**
+     *  TODO: 初始化逻辑处理
+     *    1、后端是否启动 启动则判断是否登录
+     *    2、后端没有启动的情况下应从
+     */
+
+    if (!this.isLogin) {
+
+      this.router.navigateByUrl('/login');
+      setTimeout(() => {
+        this.showLoading = true;
+        this.connectModel = 'websocket';
+      }, 2000);
+      // this.router.navigateByUrl('/learnadmin');
+    } else {
+      this.showLoading = true;
+      this.connectModel = 'http';
+      // this.router.navigateByUrl('/login');
+      this.router.navigateByUrl('/learnadmin');
+    }
   }
 }
