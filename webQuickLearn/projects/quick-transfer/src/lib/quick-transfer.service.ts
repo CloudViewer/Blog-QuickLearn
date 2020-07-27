3ee86af38267222e6a51420d95013e7be8fd9315
Ale <az_ale@163.com>
2020-07-27 12:05:34 +0800

    对quick库进行修改

--- webQuickLearn/projects/quick-transfer/src/lib/quick-transfer.service.ts
+++ webQuickLearn/projects/quick-transfer/src/lib/quick-transfer.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuickTransferService {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  public redirectToUrl(url: string): void {
+    this.router.navigateByUrl(url);
+  }
+
+
 }
