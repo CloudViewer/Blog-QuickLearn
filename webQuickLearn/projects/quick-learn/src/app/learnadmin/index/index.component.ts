b100f6c1907bb0819b6129dfc7cdc16a2c3adb16
Ale <az_ale@163.com>
2020-07-30 09:46:38 +0800

    菜单生成方式更改为Template模式

--- webQuickLearn/projects/quick-learn/src/app/learnadmin/index/index.component.ts
+++ webQuickLearn/projects/quick-learn/src/app/learnadmin/index/index.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NzDrawerService, NzModalService } from 'ng-zorro-antd';
-import { MenuComponent } from './menu/menu.component';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -12,12 +11,9 @@ import { Router } from '@angular/router';
 })
 export class IndexComponent implements OnInit {
 
-  // 引入子组件
-  @ViewChild('menuTree', { static: false })
-  public menuTree: MenuComponent;
 
-  // 左侧菜单栏布局 0 0 200px; 默认宽度200px 不增长 不缩小
-  style: string = '0 0 200px';
+  // 左侧菜单栏布局 0 0 240px; 默认宽度200px 不增长 不缩小
+  style: string = '0 0 240px';
 
   // 菜单数据
   menuData: any;
@@ -25,314 +21,357 @@ export class IndexComponent implements OnInit {
   isCollapsed: boolean = false;
 
 
-  constructor(private drawerService: NzDrawerService, private modalService: NzModalService,private router: Router) { }
+  constructor(private drawerService: NzDrawerService, private modalService: NzModalService, private router: Router) { }
 
 
   ngOnInit() {
-   
-    // 初始菜单加载
-    const menu = [
+    this.drawInit();
+  }
+
+  /**
+   * 数据初始化
+   */
+  drawInit() {
+    const menus = [
       {
+        title: '主页',
         mid: 1,
         fid: 0,
-        title: '主页',
         icon: {
           type: 'home',
-          theme: 'outline'
+          theme: ''
         },
-        code: 'HOME',
-        routerUrl: 'learnadmin/home',
+        routerParam: 'ADMIN_Home',
+        routerLink: 'learnadmin/home',
         children: [],
-        isSubmenu: false,
+        isSelected: false,
+        isDisabled: false,
       },
       {
+        title: '用户管理',
         mid: 2,
         fid: 0,
-        title: '用户管理',
         icon: {
           type: 'user',
           theme: ''
         },
-        code: '',
+        routerLink: '',
         children: [
           {
+            title: '用户列表',
             mid: 3,
             fid: 2,
-            title: '用户列表',
-            icon: {},
-            code: 'USER_LIST',
-            routerUrl: 'learnadmin/user/list',
+            icon: {
+              type: '',
+              theme: ''
+            },
+            routerParam: 'ADMIN_USER_LIST',
+            routerLink: 'learnadmin/user/list',
             children: [],
-            isSubmenu: false,
+            isSelected: false,
+            isDisabled: false,
           },
           {
+            title: '待开发...',
             mid: 4,
             fid: 2,
-            title: '待开发....',
-            icon: {},
-            code: 'NO',
-            routerUrl: 'learnadmin/user/no',
+            icon: {
+              type: '',
+              theme: ''
+            },
+            routerParam: 'ADMIN_USER_NONE',
+            routerLink: 'learnadmin/user/none',
             children: [],
-            isSubmenu: false,
-          },
-
+            isSelected: false,
+            isDisabled: false,
+          }
         ],
-        isSubmenu: true,
+        isOpen: true,
+        isDisabled: false,
       },
       {
+        title: '博客管理',
         mid: 5,
         fid: 0,
-        title: '博客管理',
         icon: {
           type: 'form',
           theme: ''
         },
-        code: '',
+        routerLink: '',
         children: [
           {
+            title: '博客列表',
             mid: 6,
             fid: 5,
-            title: '博客列表',
-            icon: {},
-            code: 'BLOG_LIST',
-            routerUrl: 'learnadmin/blog/list',
+            icon: {
+              type: '',
+              theme: ''
+            },
+            routerParam: 'ADMIN_BLOG_LIST',
+            routerLink: 'learnadmin/blog/list',
             children: [],
-            isSubmenu: false,
+            isSelected: false,
+            isDisabled: false,
           },
           {
+            title: '待审核...',
             mid: 7,
             fid: 5,
-            title: '待审核',
-            icon: {},
-            code: 'BLOG_AUDIT_LIST',
-            routerUrl: 'learnadmin/blog/audit',
+            icon: {
+              type: '',
+              theme: ''
+            },
+            routerParam: 'ADMIN_BLOG_AUDIT',
+            routerLink: 'learnadmin/blog/audit',
             children: [],
-            isSubmenu: false,
-          },
-
+            isSelected: false,
+            isDisabled: false,
+          }
         ],
-        isSubmenu: true,
+        isOpen: false,
+        isDisabled: false,
       },
       {
+        title: '模板管理',
         mid: 8,
         fid: 0,
-        title: '模板管理',
         icon: {
           type: 'edit',
           theme: ''
         },
-        code: '',
+        routerLink: '',
         children: [
           {
+            title: '模板列表',
             mid: 9,
             fid: 8,
-            title: '模板列表',
-            icon: {},
-            code: 'TEMPLATE_LIST',
-            routerUrl: 'learnadmin/template/list',
+            icon: {
+              type: '',
+              theme: ''
+            },
+            routerParam: 'ADMIN_TEMPLATE_LIST',
+            routerLink: 'learnadmin/template/list',
             children: [],
-            isSubmenu: false,
+            isSelected: false,
+            isDisabled: false,
           },
           {
+            title: '待开发...',
             mid: 10,
             fid: 8,
-            title: '待开发...',
-            icon: {},
-            code: 'NO',
-            routerUrl: 'learnadmin/template/no',
+            icon: {
+              type: '',
+              theme: ''
+            },
+            routerParam: 'ADMIN_TEMPLATE_NONE',
+            routerLink: 'learnadmin/template/none',
             children: [],
-            isSubmenu: false,
-          },
-
+            isSelected: false,
+            isDisabled: false,
+          }
         ],
-        isSubmenu: true,
+        isOpen: false,
+        isDisabled: false,
       },
       {
+        title: '广告管理',
         mid: 11,
         fid: 0,
-        title: '广告管理',
         icon: {
           type: 'notification',
           theme: ''
         },
-        code: '',
+        routerLink: '',
         children: [
           {
+            title: '广告列表',
             mid: 12,
             fid: 11,
-            title: '广告列表',
-            icon: {},
-            code: 'AD_LIST',
-            routerUrl: 'learnadmin/advert/list',
+            icon: {
+              type: '',
+              theme: ''
+            },
+            routerParam: 'ADMIN_ADVERT_LIST',
+            routerLink: 'learnadmin/advert/list',
             children: [],
-            isSubmenu: false,
+            isSelected: false,
+            isDisabled: false,
           },
           {
+            title: '广告审核',
             mid: 13,
             fid: 11,
-            title: '广告审核',
-            icon: {},
-            code: 'AD_AUDIT',
-            routerUrl: 'learnadmin/advert/audit',
+            icon: {
+              type: '',
+              theme: ''
+            },
+            routerParam: 'ADMIN_ADVERT_AUDIT',
+            routerLink: 'learnadmin/advert/audit',
             children: [],
-            isSubmenu: false,
-          },
-
+            isSelected: false,
+            isDisabled: false,
+          }
         ],
-        isSubmenu: true,
+        isOpen: false,
+        isDisabled: false,
       },
       {
+        title: '日志管理',
         mid: 14,
         fid: 0,
-        title: '日志管理',
         icon: {
           type: 'file',
           theme: ''
         },
-        code: '',
+        routerLink: '',
         children: [
           {
+            title: '登录日志',
             mid: 15,
             fid: 14,
-            title: '登录日志',
-            icon: {},
-            code: 'LOG_LOGIN_LIST',
-            routerUrl: 'learnadmin/loginlog/list',
+            icon: {
+              type: '',
+              theme: ''
+            },
+            routerParam: 'ADMIN_LOGINLOG_LIST',
+            routerLink: 'learnadmin/loginlog/list',
             children: [],
-            isSubmenu: false,
+            isSelected: false,
+            isDisabled: false,
           },
           {
+            title: '操作日志',
             mid: 16,
             fid: 14,
-            title: '操作日志',
-            icon: {},
-            code: 'LOG_OPERATE_LIST',
-            routerUrl: 'learnadmin/operatelog/list',
+            icon: {
+              type: '',
+              theme: ''
+            },
+            routerParam: 'ADMIN_OPERATELOG_LIST',
+            routerLink: 'learnadmin/operatelog/list',
             children: [],
-            isSubmenu: false,
-          },
-
+            isSelected: false,
+            isDisabled: false,
+          }
         ],
-        isSubmenu: true,
+        isOpen: false,
+        isDisabled: false,
       },
       {
+        title: '权限管理',
         mid: 17,
         fid: 0,
-        title: '权限管理',
         icon: {
           type: 'key',
           theme: ''
         },
-        code: '',
+        routerLink: '',
         children: [
           {
+            title: '账号权限',
             mid: 18,
             fid: 17,
-            title: '管理员权限',
-            icon: {},
-            code: 'ADMIN_AUTH',
-            routerUrl: 'learnadmin/admin/auth',
+            icon: {
+              type: '',
+              theme: ''
+            },
+            routerParam: 'ADMIN_ADMIN_AUTO',
+            routerLink: 'learnadmin/admin/auth',
             children: [],
-            isSubmenu: false,
+            isSelected: false,
+            isDisabled: false,
           },
           {
+            title: '菜单权限',
             mid: 19,
             fid: 17,
-            title: '菜单权限',
-            icon: {},
-            code: 'MENU_AUTH',
-            routerUrl: 'learnadmin/menu/auth',
+            icon: {
+              type: '',
+              theme: ''
+            },
+            routerParam: 'ADMIN_MENU_AUTO',
+            routerLink: 'learnadmin/menu/auth',
             children: [],
-            isSubmenu: false,
-          },
-
+            isSelected: false,
+            isDisabled: false,
+          }
         ],
-        isSubmenu: true,
+        isOpen: false,
+        isDisabled: false,
       },
       {
+        title: '其他',
         mid: 20,
         fid: 0,
-        title: '其他',
         icon: {
           type: 'setting',
           theme: ''
         },
-        code: '',
+        routerLink: '',
         children: [
           {
+            title: '账号权限',
             mid: 21,
             fid: 20,
-            title: '系统设置',
-            icon: {},
-            code: 'SYS_SETTING',
-            routerUrl: 'learnadmin/sys/setting',
+            icon: {
+              type: '',
+              theme: ''
+            },
+            routerParam: 'ADMIN_SYS_SETTING',
+            routerLink: 'learnadmin/sys/setting',
             children: [],
-            isSubmenu: false,
+            isSelected: false,
+            isDisabled: false,
           },
           {
+            title: '清除缓存',
             mid: 22,
             fid: 20,
-            title: '清除缓存',
-            icon: {},
-            code: 'SYS_CLEAR_CACHE',
-            routerUrl: 'learnadmin/clear/cache',
-            children: [
-              {
-                mid: 23,
-                fid: 22,
-                title: '测试菜单-1',
-                icon: {},
-                code: 'SYS_CLEAR_CACHE',
-                routerUrl: 'learnadmin/clear/cache',
-                children: [],
-                isSubmenu: false,
-              },
-              {
-                mid: 24,
-                fid: 22,
-                title: '测试菜单-2',
-                icon: {},
-                code: 'SYS_CLEAR_CACHE',
-                routerUrl: 'learnadmin/clear/cache',
-                children: [
-                  {
-                    mid: 25,
-                    fid: 24,
-                    title: '测试菜单-2-1',
-                    icon: {},
-                    code: 'SYS_CLEAR_CACHE',
-                    routerUrl: 'learnadmin/clear/cache',
-                    children: [],
-                    isSubmenu: false,
-                  },
-                ],
-                isSubmenu: true,
-              },
-            ],
-            isSubmenu: true,
-          },
-
+            icon: {
+              type: '',
+              theme: ''
+            },
+            routerParam: 'ADMIN_CLEAR_CACHE',
+            routerLink: 'learnadmin/clear/cache',
+            children: [],
+            isSelected: false,
+            isDisabled: false,
+          }
         ],
-        isSubmenu: true,
+        isOpen: false,
+        isDisabled: false,
       }
-
     ];
-    this.menuData = menu;
-    console.log('index..')
-    const nowUrl = this.router.url;
-    const lastIndex = nowUrl.lastIndexOf('/');
-    const urlCode = nowUrl.substring(lastIndex + 1, nowUrl.length);
-    console.log(urlCode)
+
+    this.menuData = menus;
+    this.findByRouterRS(menus, this.router.url.slice(1, -1));
+  }
+
+  /**
+   * 递归查找路由
+   */
+  findByRouterRS(menus: any[], link: string) {
+    console.log(menus);
+    console.log(link)
+
   }
 
   /**
    * 菜单设置
    */
   doMenuSetting() {
-    this.menuTree.doMenuSetting();
-    this.isCollapsed = this.menuTree.isCollapsed;
-    this.style = this.isCollapsed ? '0 0 90px' : '0 0 200px'
+    // this.menuTree.doMenuSetting();
+    this.isCollapsed = this.isCollapsed ? false : true;
+    this.style = this.isCollapsed ? '0 0 90px' : '0 0 240px'
   }
+
+
+  subMenuOpenChange(item: any) {
+    console.log('click')
+    console.log(item)
+  }
+
+
 
 
 
