15c23bc785b3752536d11a327f43f3ec678d2322
Ale <az_ale@163.com>
2020-08-31 10:48:23 +0800

    模块调整

--- webQuickLearn/projects/quick-learn/src/app/learncomm/learncomm.module.ts
+++ webQuickLearn/projects/quick-learn/src/app/learncomm/learncomm.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CommIconComponent } from './comm-icon/comm-icon.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CommIconComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [CommIconComponent],
 })
 export class LearncommModule { }
