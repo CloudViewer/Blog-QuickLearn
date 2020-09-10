ce5b8b59f49556d4e83cd8b0cfb133e509887e11
Ale <az_ale@163.com>
2020-09-10 14:29:10 +0800

    后端环境搭建（测试版本），后续架构调整。

---
+++ ideaQunickLearn/Ale-QuickLearnWeb/src/main/java/com/learn/es/comm/config/cors/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.learn.es.comm.config.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Ale on 2020/9/7
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    /**
+     * 前后端跨域处理
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(3600);
+    }
+}
